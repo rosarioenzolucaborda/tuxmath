d0193f626d9469838743ec9ac1957bcab886e777
Julien Marin <julien.marin@afrikalan.org>
2021-10-07 20:04:53 +0000

    add licence

--- js/gameElements.js
+++ js/gameElements.js
@@ -1,3 +1,22 @@
+// TuxMath JS
+//
+// Author: Julien Marin - projet Afrikalan  
+// Contact: julien.marin@afrikalan.org
+// Licence: AGPL-3.0-or-later, see COPYING file
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 const GAME_COLUMNS_NUMBER=4;
 const COMTE_FALL_TIME=50;
 
